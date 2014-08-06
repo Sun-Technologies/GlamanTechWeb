463449d0cc12f3042103059de3b3ff4d2486abd7
chandramouleshwar <chandra.shalwi@gmail.com>
2014-08-06 11:58:58 -0400

    comment box and image calendar has been updated

--- web/whitepapers.php
+++ web/whitepapers.php
@@ -4,31 +4,39 @@
     <h1 class="page-title">White Papers</h1>
     <hr/>
     <div class="wp-area">
-    <h2><span class="date-style">Nov 4, 2013</span>&nbsp; AWS Security</h2>
+    <div class="date-area"><span><img src="images/fourth-nov.png" alt="4th November"></span><span class="date-head">AWS Security</span></div>
     <p>The AWS cloud infrastructure offers secure cloud computing environments with high flexibility, dependability and availability to the customers to build a vast range of applications. It helps in protecting confidentiality, integrity, and availability of our customers’ systems and data.</p>
     <p><span class="download-wp"><a href="http://aws.amazon.com/whitepapers/">Download Whitepaper</a></span></p>
+    <!-- Begin Comments JavaScript Code -->
+    <div class="view-content"> 
+    <p><script type="text/javascript" async>function ajaxpath_53e220c37cd07(url){return window.location.href == '' ? url : url.replace('&s=','&s=' + escape(window.location.href));}
+    (function(){document.write('<div id="fcs_div"><a title="free comment script" href="http://www.freecommentscript.com">&nbsp;&nbsp;<b>Comment Loading......</b>...</a></div>');
+    fcs_53e220c37cd07=document.createElement('script');fcs_53e220c37cd07.type="text/javascript";fcs_53e220c37cd07.src=ajaxpath_53e220c37cd07("http://www.freecommentscript.com/GetComments.php?p=53e220c37cd07&s=&Size=10#!53e220c37cd07");
+    setTimeout("document.getElementById('fcs_div').appendChild(fcs_53e220c37cd07)",1);})();
+    </script></p><!-- End Comments JavaScript Code -->
+    </div>
     </div>
     <div class="wp-area">
-    <h2><span class="date-style">Jul 12, 2013 </span>&nbsp; SAP HANA</h2>
+    <div class="date-area"><span><img src="images/12-jul.png" alt="4th November"></span><span class="date-head">SAP HANA</span></div>
     <p>High-Performance Analytic Appliance (HANA) is an application that uses in-memory database technology combining transactional and analytical data processing with application logic processing. It helps in processing high volume of data in real-time.</p>
     <p>To learn more about the benefits of SAP HANA download</p>
     <p><span class="download-wp"><a href="http://www.saphana.com/docs/DOC-1381">Download Whitepaper</a></span></p>
     </div>
     <div class="wp-area">
-    <h2><span class="date-style">Apr 10, 2012</span>&nbsp; Bring Your Own Device (BYOD) Unleashed in the Age of IT Consumerization (White Paper)</h2>
+    <div class="date-area"><span><img src="images/10-apr.png" alt="4th November"></span><span class="date-head">Bring Your Own Device (BYOD) Unleashed in the Age of IT Consumerization (White Paper)</span></div>
     <p>Today, employees bring their mobile devices and use them to access privileged company information and applications. It becomes difficult for IT department to understand what device is accessing their network.</p>
     <p>For more info download this whitepaper</p>
     <p><span class="download-wp"><a href="http://resources.idgenterprise.com/original/AST-0055442_BradfordWP0103_2_.pdf">Download Whitepaper</a></span></p>
     </div>
     <div class="wp-area">
-    <h2><span class="date-style">Apr 16, 2013</span>&nbsp; SAP Management with LVM &amp; VMware on IBM PureSystems</h2>
+    <div class="date-area"><span><img src="images/9th-july.png" alt="4th November"></span><span class="date-head">SAP Management with LVM &amp; VMware on IBM PureSystems</span></div>
     <p>IBM PureSystems can help clients to manage the complexity of SAP landscape, and to reduce application workloads.</p>
     <p>SAP NetWeaver Landscape Virtualization Management provides Virtualization, Storage, and Management/Orchestration (“External Interfaces”) to SAP partner technologies.</p>
     <p>For more download whitepaper</p> 
     <p><span class="download-wp"><a href="http://www-03.ibm.com/support/techdocs/atsmastr.nsf/fe582a1e48331b5585256de50062ae1c/cfa66a29871a2e6c86257a410060dc67/$FILE/Virtualization%20for%20SAP%20on%20IBM%20PureSystems%20with%20Microsoft%20Hyper-V.pdf">Download Whitepaper</a></span></p>
     </div>
     <div class="wp-area">
-    <h2><span class="date-style">Jul 9, 2009</span>&nbsp; The 7 Keys to Mobile CRM Success</h2>
+    <div class="date-area"><span><img src="images/16-apr.png" alt="4th November"></span><span class="date-head">The 7 Keys to Mobile CRM Success</span></div>
     <p>Big enterprises using mobile solution for customer relation management. Mobile CRM helps improving the data quality, saves time and boost productivity.</p>
     <p>To learn more about mobile CRM  and to get started visit </p>
     <p><span class="download-wp"><a href="http://www.salesforce.com/assets/pdf/datasheets/SalesforceMobile_Datasheet.pdf">Download Whitepaper</a></span></p>
