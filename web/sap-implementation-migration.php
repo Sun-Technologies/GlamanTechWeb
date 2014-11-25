d4bc082378d2b958b3b901e60b7fb2a70746f826
tahirimran <tahiri@suntechnologies.com>
2014-11-25 18:45:55 +0530

    styles

--- web/sap-implementation-migration.php
+++ web/sap-implementation-migration.php
@@ -7,7 +7,7 @@
         <p>Whether you are implementing SAP for the first time or adding additional module, expertise of GlamanTech can assist you with structured and proven approach in planning, designing, implementing, maintaining and optimizing business processes, and SAP solutions.</p>
         <p>We also offer a wide range of SAP solutions to clients in almost all industries around the globe to match the business processes of the industry.</p>
         <p>Our mission is to help you innovate, transform, enhance and optimize your business with flexibility and responsiveness from a both local and global partner with superior solutions and global delivery</p>
-        <h3>We provide on time and in budget service to improve your business process by offering services at every step of the Life cycle of SAP ERP:</h3>
+        <p>We provide on time and in budget service to improve your business process by offering services at every step of the Life cycle of SAP ERP:</p>
         <ul class="oracle-content">
           <li>SAP installation</li>
           <li>Implementation</li>
