a8031777f161799c54d0f58f62a6996cb0ce65dc
priya144 <priyankay@suntechnologies.com>
2014-12-01 18:27:54 +0530

    Home page font style and size

--- web/footer.php
+++ web/footer.php
@@ -2,18 +2,18 @@
     <div id="footer-area" class="clearfix">
       <div class="region region-footer">
         <div id="block-block-5" class="block block-block animated fadeInLeft">
-          <h2 ><span><span><img src="images/flags/Planning_icon.png" alt="Planning" class="flags"></span>Planning</span></h2>
-          <div class="content">GlamanTech offers its expertise in the all the areas of implementation, starting from a complete understanding of the client's requirements and including business process comprehension, mapping of requirements, design and development, database technologies, development tools or languages, and implementation of the application.</div>
+          <h2 style="font-family: Century Gothic;"><span style="color:#00B2EE"><span><img src="images/flags/Planning_icon.png" alt="Planning" class="flags"></span>Planning</span></h2>
+          <div class="content" style="font-family: Calibri;font-size:1.2em;">GlamanTech offers its expertise in the all the areas of implementation, starting from a complete understanding of the client's requirements and including business process comprehension, mapping of requirements, design and development, database technologies, development tools or languages, and implementation of the application.</div>
         </div>
         <!-- /.block -->
         <div id="block-contactinfo-hcard" class="block block-contactinfo animated fadeInDown">
-          <h2 ><span><span><img src="images/flags/Strategy_icon.png" alt="Strategy" class="flags"></span>Strategy</span></h2>
-          <div class="content">We focus on the special needs, resources and capabilities of our clients. We capture the client’s cultural and environmental differences to offer the best business application solution. We provide our expertise to help your team to build a model for the growth and success of your organization.</div>
+          <h2 style="font-family: Century Gothic;"><span style="color:#00B2EE"><span><img src="images/flags/Strategy_icon.png" alt="Strategy" class="flags"></span>Strategy</span></h2>
+          <div class="content" style="font-family: Calibri;font-size:1.2em;">We focus on the special needs, resources and capabilities of our clients. We capture the client’s cultural and environmental differences to offer the best business application solution. We provide our expertise to help your team to build a model for the growth and success of your organization.</div>
         </div>
         <!-- /.block -->
         <div id="block-simplenews-1" class="block block-simplenews animated fadeInRight">
-          <h2 ><span><span><img src="images/flags/Result_icon.png" alt="Result" class="flags"></span>Result</span></h2>
-          <div class="content">We focus on delivering services that improve business efficiency. Many clients have benefitted by augmenting their staff resources by using our services for short term as well as long term projects.</div>
+          <h2 style="font-family: Century Gothic;"><span style="color:#00B2EE"><span><img src="images/flags/Result_icon.png" alt="Result" class="flags"></span>Result</span></h2>
+          <div class="content" style="font-family: Calibri;font-size:1.2em;" >We focus on delivering services that improve business efficiency. Many clients have benefitted by augmenting their staff resources by using our services for short term as well as long term projects.</div>
         </div>
         <!-- /.block -->  
       </div>
