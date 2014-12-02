8ad145284b63b7289af403b40e9e1dd2b0107109
priya144 <priyankay@suntechnologies.com>
2014-12-02 16:16:35 +0530

    Removed div with id tag for Terms and condition

--- web/terms_and_conditions.php
+++ web/terms_and_conditions.php
@@ -1,83 +1,56 @@
 <?php $CURRENT_PAGE= "resources"; ?>
 <?php include'header.php'; ?>
   <div id="primary" class="container   clearfix animated bounceIn">
-    <div id="resume-tips">
       <h1 id="side-nav" class="page-title navbar-example">Terms And Conditions</h1><hr>
+      <div class="wp-area">
+        <p>PLEASE READ THE FOLLOWING TERMS AND CONDITIONS CAREFULLY. THEY GOVERN YOUR ACCESS AND USE OF THIS WEBSITE. BY ACCESSING OR USING THIS WEBSITE, YOU AGREE TO BE BOUND BY THESE TERMS AND CONDITIONS AND ACCEPT THEM IN FULL, AS THEY MAY BE MODIFIED BY GLAMAN TECH INC. FROM TIME-TO-TIME AND POSTED ON THIS WEBSITE.</p>
+      </div>
        
-      <div class="wp-area">
-      <p>PLEASE READ THE FOLLOWING TERMS AND CONDITIONS CAREFULLY. THEY GOVERN YOUR ACCESS AND USE OF THIS WEBSITE. BY ACCESSING OR USING THIS WEBSITE, YOU AGREE TO BE BOUND BY THESE TERMS AND CONDITIONS AND ACCEPT THEM IN FULL, AS THEY MAY BE MODIFIED BY GLAMAN TECH INC. FROM TIME-TO-TIME AND POSTED ON THIS WEBSITE.</p>
-       </div>
-    </div>
-    <div id="no-warranties" >
       <h3>No Warranties</h3>
-      
-     <div class="wp-area">
-      <p>Although Glaman Tech attempts to provide accurate information, names, images, pictures, logos, icons, documents, and materials (collectively, the "Contents") on the Website, it makes no representation, endorsement, or warranty that such Contents are accurate or suitable for any particular purpose.</p>
-      <p>THE WEBSITE AND ITS CONTENTS ARE PROVIDED ON AN "AS IS" BASIS. USE OF THE WEBSITE AND ITS CONTENTS IS AT THE USER'S SOLE RISK. THE WEBSITE AND ITS CONTENTS ARE PROVIDED WITHOUT ANY REPRESENTATIONS, ENDORSEMENTS, OR WARRANTIES OF ANY KIND WHATSOEVER, EITHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY WARRANTIES OF TITLE OR ACCURACY AND ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT, WITH THE SOLE EXCEPTION OF WARRANTIES (IF ANY) WHICH CANNOT BE EXPRESSLY EXCLUDED UNDER APPLICABLE LAW. AS NOTED BELOW, GLAMAN TECH ALSO MAKES NO REPRESENTATIONS, ENDORSEMENTS, OR WARRANTIES, EITHER EXPRESS OR IMPLIED, WITH RESPECT TO ANY WEBSITE OPERATED BY A THIRD PARTY.</p>
-      </div>
-    </div>
-    <div id="limitations">
-      <h3>Limitation of Liability</h3>
-      
       <div class="wp-area">
-       <p>In no event will Glaman Tech be liable for any damages, including, without limitation, indirect, incidental, special, consequential or punitive damages, whether under a contract, tort or any other theory of liability, arising in connection with any party's use of the website or in connection with any failure of performance, error, omission, interruption, defect, delay in operation or transmission, computer virus, line system failure, loss of data, or loss of use related to this website or any website operated by any third party or any contents of this website or any other website, even if Glaman Tech is aware of the possibility of such damages.</p>
+        <p>Although Glaman Tech attempts to provide accurate information, names, images, pictures, logos, icons, documents, and materials (collectively, the "Contents") on the Website, it makes no representation, endorsement, or warranty that such Contents are accurate or suitable for any particular purpose.</p>
+        <p>THE WEBSITE AND ITS CONTENTS ARE PROVIDED ON AN "AS IS" BASIS. USE OF THE WEBSITE AND ITS CONTENTS IS AT THE USER'S SOLE RISK. THE WEBSITE AND ITS CONTENTS ARE PROVIDED WITHOUT ANY REPRESENTATIONS, ENDORSEMENTS, OR WARRANTIES OF ANY KIND WHATSOEVER, EITHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY WARRANTIES OF TITLE OR ACCURACY AND ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT, WITH THE SOLE EXCEPTION OF WARRANTIES (IF ANY) WHICH CANNOT BE EXPRESSLY EXCLUDED UNDER APPLICABLE LAW. AS NOTED BELOW, GLAMAN TECH ALSO MAKES NO REPRESENTATIONS, ENDORSEMENTS, OR WARRANTIES, EITHER EXPRESS OR IMPLIED, WITH RESPECT TO ANY WEBSITE OPERATED BY A THIRD PARTY.</p>
       </div>
-    </div>
-
-     <div id="Links to Other Websites">
+   
+   
+      <h3>Limitation of Liability</h3>
+      <div class="wp-area">
+        <p>In no event will Glaman Tech be liable for any damages, including, without limitation, indirect, incidental, special, consequential or punitive damages, whether under a contract, tort or any other theory of liability, arising in connection with any party's use of the website or in connection with any failure of performance, error, omission, interruption, defect, delay in operation or transmission, computer virus, line system failure, loss of data, or loss of use related to this website or any website operated by any third party or any contents of this website or any other website, even if Glaman Tech is aware of the possibility of such damages.</p>
+      </div>
+    
       <h3>Use of Website</h3>
-      
       <div class="wp-area">
        <p>Contents are included in this Website solely for the personal use of Website users. You may not copy (other than a copy for personal use), modify, distribute, transmit, display, perform, reproduce, transfer, resell, or republish any of the Contents of this Website without the prior written consent of Glaman Tech, which may be withheld in its sole discretion.</p>
       </div>
-    </div>
-
-     <div id="limitations">
+    
       <h3>Copyrights and Other Intellectual Property</h3>
-      
       <div class="wp-area">
        <p>Except where otherwise expressly noted or as noted below, all Contents of this Website, including white papers, case studies, graphics, icons and overall appearance of the Website, are the sole and exclusive properties of Glaman Tech.</p>
       </div>
-    </div>
-
-     <div id="limitations">
+   
       <h3>Links to Other Websites</h3>
-      
       <div class="wp-area">
        <p>Glaman Tech may establish links between this Website and one or more Websites operated by third parties. Glaman Tech has no control over any such other Websites, the contents therein or the products/services offered. The existence of any such links shall not constitute an endorsement, representation or warranty by Glaman Tech of such Websites, the contents of the Websites, the products or services of the Websites or the operators of the Websites. Glaman Tech disclaims responsibility for the privacy policies and customer information practices of third-party internet sites hyperlinked to our sites.</p>
       </div>
-    </div>
-
-    <div id="limitations">
+    
       <h3>Modifications</h3>
-      
       <div class="wp-area">
-       <p>Glaman Tech may, at any time, make modifications, changes, and alterations to the Contents of this Website, including these Terms and Conditions, without prior notice. You are responsible for regularly reviewing these terms and conditions. Your continued use of this Website following any modifications, changes, or alterations shall constitute your acceptance of such modifications, changes, or alterations.</p>
+        <p>Glaman Tech may, at any time, make modifications, changes, and alterations to the Contents of this Website, including these Terms and Conditions, without prior notice. You are responsible for regularly reviewing these terms and conditions. Your continued use of this Website following any modifications, changes, or alterations shall constitute your acceptance of such modifications, changes, or alterations.</p>
       </div>
-    </div>
-
-     <div id="limitations">
+        
       <h3>Waiver</h3>
-      
       <div class="wp-area">
-       <p>No waiver by Glaman Tech of any right under or term or provision of these Terms and Conditions will be deemed a waiver of any other right, term, or provision of these Terms and Conditions at that time or a waiver of that or any other right, term, or provision of these Terms and Conditions at any other time.</p>
+        <p>No waiver by Glaman Tech of any right under or term or provision of these Terms and Conditions will be deemed a waiver of any other right, term, or provision of these Terms and Conditions at that time or a waiver of that or any other right, term, or provision of these Terms and Conditions at any other time.</p>
       </div>
-    </div>
-
-     <div id="limitations">
+    
       <h3>Contact</h3>
-      
       <div class="wp-area">
-       <p>You may contact Glaman Tech by e-mail at admin@glamantech.com</p>
+        <p>You may contact Glaman Tech by e-mail at admin@glamantech.com</p>
       </div>
-    </div>
 
-     <div id="limitations">
       <h3>Privacy Policy</h3>
-      
       <div class="wp-area">
-       <p>Glaman Tech and its affiliates are committed to protecting your privacy. This Privacy Policy applies to activities directly related to your visit of the Glaman Tech websites. It does not apply to third parties websites linked through Glaman Tech website. We strongly recommend that you read these third parties privacy policies prior to providing any information to such sites. Glaman Tech has no responsibility or liability in connection with any third parties websites.</p>
+        <p>Glaman Tech and its affiliates are committed to protecting your privacy. This Privacy Policy applies to activities directly related to your visit of the Glaman Tech websites. It does not apply to third parties websites linked through Glaman Tech website. We strongly recommend that you read these third parties privacy policies prior to providing any information to such sites. Glaman Tech has no responsibility or liability in connection with any third parties websites.</p>
       </div>
-    </div>
-  </div>
+   </div>
   <?php include'footer1.php';?>
