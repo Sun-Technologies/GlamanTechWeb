faf324a942c1b3826a7d6b3cf82d8c1cf2ea11c3
priya144 <priyankay@suntechnologies.com>
2014-12-02 13:58:13 +0530

    edited font family tag

--- web/footer1.php
+++ web/footer1.php
@@ -2,20 +2,20 @@
       <div class="footer-third">
         <ul>
           <li>
-            <h3 style="font-family: Century Gothic;">Visit</h3>
-            <p style="font-family: Calibri; font-size:1em;">5525 N MacArthur Blvd,<br>Suite #240 Irving,TX,75038.</p>
+            <h3 class="header-font-style">Visit</h3>
+            <p class="content-font-style">5525 N MacArthur Blvd,<br>Suite #240 Irving,TX,75038.</p>
           </li>
           <li>
-            <h3 style="font-family: Century Gothic;">Call</h3>
-            <p style="font-family: Calibri; font-size:1em;">Phone No:972-992-0770 Ext100<br>Fax:817-423-6838</p>
+            <h3 class="header-font-style">Call</h3>
+            <p class="content-font-style">Phone No:972-992-0770 Ext100<br>Fax:817-423-6838</p>
           </li>
           <li>
-            <h3 style="font-family: Century Gothic;">Contact</h3>
-            <p style="font-family: Calibri; font-size:1em;">Email: hr@glamantech.com<br>Website:www.glamantech.com</p>
+            <h3 class="header-font-style">Contact</h3>
+            <p class="content-font-style">Email: hr@glamantech.com<br>Website:www.glamantech.com</p>
           </li>
           <li>
-            <h3 style="font-family: Century Gothic;"><span>&copy;</span>&nbsp;<?php echo date("Y");?> Glaman Tech</h3>
-            <p style="font-family: Calibri; font-size:1em;">All Rights Reserved<br><a href="terms_and_conditions.php">Terms and conditions</a></p>
+            <h3 class="header-font-style"><span>&copy;</span>&nbsp;<?php echo date("Y");?> Glaman Tech</h3>
+            <p class="content-font-style">All Rights Reserved<br><a href="terms_and_conditions.php">Terms and conditions</a></p>
           </li>
         </ul>
       </div>
