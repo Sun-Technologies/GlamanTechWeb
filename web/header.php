07acf9e48c40cd258f95ac4011e7ac829c848973
priya144 <priyankay@suntechnologies.com>
2014-12-03 11:05:09 +0530

    Updated content and image for solution manager

--- web/header.php
+++ web/header.php
@@ -128,7 +128,7 @@ jQuery(document).ready(function($) {$('headerimgs').bgimgSlideshow({photos : [{
                     <li><a title="SAP Customer Relationship Management" href="sap-crm.php">SAP Customer Relationship Management</a></li>
                     <li><a title="SAP Supplier Relationship Management" href="sap-srm.php">SAP Supplier Relationship Management</a></li>
                     <li><a title="SAP Supply Chain Management" href="sap-scm.php">SAP Supply Chain Management</a></li>
-                    <li><a title="SAP Solution Manager with CHaRM" href="sap-scmsolution-manager.php">SAP Solution Manager with CHaRM</a></li>
+                    <li><a title="SAP Solution Manager with CHaRM" href="sap-solution-manager.php">SAP Solution Manager with CHaRM</a></li>
                     <li><a title="SAP Mobility" href="sap-mobility.php">SAP Mobility</a></li>
                     <li><a title="SAP HANA" href="sap-hana.php">SAP HANA</a></li>
                     <li><a title="SAP AMS" href="sap-ams.php">SAP AMS</a></li>
--- web/sap-solution-manager.php
+++ web/sap-solution-manager.php
@@ -1,11 +1,11 @@
 <?php $CURRENT_PAGE= "services" ?>
 <?php include'header.php'; ?>
   <div id="primary" class="container   clearfix" style="padding-bottom: 4%;">
-    <h1 id="side-nav">SAP Solution Manager</h1>
+    <h1 id="side-nav">SAP Solution Manager With CHaRM</h1>
        <hr/>
       <div class="wp-area">
-        <p>The SAP Solution Manager is the central and integrated application management platform allowing business processes to be implemented, operated, and monitored for SAP as well as non-SAP systems, thus stream lining all our activities.</p>
-        <p>SAP Solution Manager supports you through each phase of your project, from planning to testing and hand-over to operations and helps optimize operations once implementation is complete.</p>
+        <p>The SAP Solution Manager is the central implementation and management platform for SAP applications, allowing business processes to be implemented, operated, and monitored for SAP as well as non-SAP systems, thus stream lining all our activities</p>
+        <p>Solution Manager provides capabilities to manage change requests through SAP’s Change Request Management, commonly referred to as ChaRM, offers greater efficiency, automation and control with a predefined set of workflows and processes. It provides tracking and documentation of all change requests and transports for business solution development in an organization.</p>
         <h3>What we do?</h3>
         <ul class="oracle-content">
           <li>IT Service Management</li>
@@ -21,14 +21,18 @@
         </ul>
         <h3>Benefits:</h3>
         <ul class="oracle-content">
-          <li>Minimize risk during implementation</li>
+          <li>Automatic tracking mechanism for every change</li>
           <li>Reduced cost of implementation and continuous improvement</li>
-          <li>Faster ROI</li>
-          <li>Reduced cost of operation</li>
+          <li>Fast and direct changes in the various stages of project</li>
+          <li>Comprehensible documentation of planned changes and their consequences</li>
+          <li>It ensures that changes are not disrupting ongoing business</li>
+          <li>Increase in the transparency of your solution</li>
           <li>Ease of implementation and upgrades</li>
           <li>Faster implementation</li>
-          <li>Central point of control for the entire solution landscape</li>
         </ul>
+      </div>
+      <div>
+        <img src="images/sap_ solution_manager.png" alt="SAP SCM"  style="width: 40%; margin-left:25%;">
       </div>
     </div>
   <?php include'footer1.php'; ?>
