e7c51078479c723155375b113f4c916354b76af7
priya144 <priyankay@suntechnologies.com>
2014-12-04 18:23:51 +0530

    SAP Solution page layout

--- web/sap-solutions.php
+++ web/sap-solutions.php
@@ -1,22 +1,217 @@
 <?php $CURRENT_PAGE= "services" ?>
 <?php include'header.php'; ?>
   <div id="primary" class="container   clearfix animated bounceIn">
-   <h1 id="side-nav">SAP Solutions:</h1>
+   <h1 id="side-nav" style="color;">SAP Solutions</h1>
        <hr/>
-      <div class="wp-area">
-        <ul class="oracle-content">
-          <li>SAP ERP</li>
-          <li>SAP Business Intelligence  and Analytics</li>
-          <li>SAP Mobility Solutions</li>
-          <li>SAP Line of Business (CRM,SCM,SRM)</li>
-          <li>Human Capital Management</li>
-          <li>ERP Suite for Transit Agencies</li>
-          <li>Practical Manufacturing Execution (PME) powered by SAP MII</li>
-          <li>Practical Manufacturing Operations and Control (PMOC) powered by SAP ME</li>
-          <li>Data Center ready SAP HANA</li>
-          <li>SAP IS-U Center of Excellence</li>
+
+        <ul class="img-list">
+           <li>
+            <a href="sap-implementation-migration.php">
+             <img src="images/migration.png"  />
+             <h2 class="header-font-style">SAP Migration
+             <span class="text-content"><span>CLICK HERE TO KNOW HOW WE CAN BENEFIT YOU</span></span></h2>
+            </a>
+          </li>
+          <li >
+            <p class="header-font-style">We help you innovate, transform, enhance and optimize your business.</p>
+          </li>
+          <li>
+             <a href="https://www.youtube.com/watch?v=p6wQl2I_p6s">
+              <img src="images/image1.png"/>
+              <h2 class="header-font-style">Learn More
+              <span class="text-content"><span>Don't know what it is?
+                <p> Watch video here</span></span></h2>
+             </a>
+          </li>
         </ul>
-        <img src="images/sap-solutions.png" alt="SAP Solutions Image" style="width: 60%; height: auto; padding-left: 20%;">
-      </div>
-    </div>
-  <?php include'footer1.php'; ?>
+    
+      
+        <ul class="img-list">
+           <li>
+            <a href="https://www.youtube.com/watch?v=lYCEQqSM08I">
+             <img src="images/image1.png"  />
+            <h2 class="header-font-style">Learn More
+              <span class="text-content"><span>Don't know what it is?
+              <p> Watch video here</span></span></h2>
+             </a>
+          </li>
+          <li>
+             <p class="header-font-style">We are trusted, reliable, experienced partner and are committed to implementing SAP ERP</p>
+          </li>
+          <li>
+             <a href="sap-erp.php">
+              <img src="images/image.png"/>
+              <h2 class="header-font-style"> SAP ERP
+              <span class="text-content"><span>CLICK HERE TO KNOW HOW WE CAN BENEFIT YOU</span></span></h2>
+             </a>
+          </li>
+        </ul>
+  
+        <ul class="img-list">
+          <li>
+             <a href="sap-crm.php">
+             <img src="images/crm.png"  />
+             <h2 class="header-font-style">SAP CRM
+             <span class="text-content"><span>CLICK HERE TO KNOW HOW WE CAN BENEFIT YOU</span></span></h2>
+            </a>     </li>  
+          <li>
+             <p class="header-font-style">We tailor the CRM user experience to match your needs.</p>
+          </li>
+         <li>
+             <a href="https://www.youtube.com/watch?v=uzuSZSKgdMQ">
+              <img src="images/image1.png"/>
+              <h2 class="header-font-style">Learn More
+              <span class="text-content"><span>Don't know what it is?
+              <p> Watch video here</span></span></h2>
+             </a>
+          </li>
+          </ul>
+       
+        <ul class="img-list">
+          <li>
+            <a href="https://www.youtube.com/watch?v=ffnRHEC0oys">
+              <img src="images/image1.png"/>
+              <h2 class="header-font-style"> Learn More
+              <span class="text-content"><span>Don't know what it is?
+              <p> Watch video here</span></span></h2>
+            </a> 
+          </li>  
+          <li>
+             <p class="header-font-style">SAP SRM is implemented by using proven methodologies, proven technology and best practices to help you</p>
+          </li>
+         <li>
+           <a href="sap-srm">
+              <img src="images/srm1.png"/>
+              <h2 class="header-font-style"> SAP SRM
+              <span class="text-content"><span>CLICK HERE TO KNOW HOW WE CAN BENEFIT YOU</span></span></h2>
+             </a> 
+          </li>
+          </ul>
+       
+          <ul class="img-list">
+          <li>
+           <a href="sap-scm.php">
+             <img src="images/scm.png"  />
+             <h2 class="header-font-style">SAP SCM
+             <span class="text-content"><span>CLICK HERE TO KNOW HOW WE CAN BENEFIT YOU</span></span></h2>
+            </a>  
+          </li>  
+          <li>
+             <p class="header-font-style">We offer reliable and sustainable operations.</p>
+          </li>
+         <li>
+          <a href="https://www.youtube.com/watch?v=Mi1QBxVjZAw">
+            <img src="images/image1.png"/>
+            <h2 class="header-font-style"> Learn More
+            <span class="text-content"><span>Don't know what it is?
+            <p> Watch video here</span></span></h2>
+          </a> 
+          </li>
+          </ul>
+       
+          <ul class="img-list">
+          <li>
+            <a href="https://www.youtube.com/watch?v=HGE0pXSJj3A">
+             <img src="images/image1.png"/>
+             <h2 class="header-font-style"> Learn More
+             <span class="text-content"><span>Don't know what it is?
+             <p> Watch video here</span></span></h2>
+            </a>
+          </li> 
+          <li>
+             <p class="header-font-style">Our SAP SCM team support you to manage integrated business processes.</p>
+          </li>
+          <li>
+             <a href="sap-solution-manager.php">
+              <img src="images/solman.png"/>
+             <h2 class="header-font-style">SAP SolMan
+              <span class="text-content"><span>CLICK HERE TO KNOW HOW WE CAN BENEFIT YOU</span></span></h2>
+             </a>
+          </li> 
+          </ul>
+      
+          <ul class="img-list">
+          <li>
+             <a href="sap-mobility.php">
+              <img src="images/mobility.png"/>
+             <h2 class="header-font-style"> SAP Mobility
+              <span class="text-content"><span>CLICK HERE TO KNOW HOW WE CAN BENEFIT YOU</span></span></h2>
+             </a>
+          </li>  
+          <li>
+             <p class="header-font-style">Well-established partner to guide and help you in understanding the complex roadmap of enterprise mobility.</p>
+          </li>
+         <li>
+            <a href="https://www.youtube.com/watch?v=sgWLHWyyP1M">
+             <img src="images/image1.png"  />
+             <h2 class="header-font-style"> Learn More
+             <span class="text-content"><span>Don't know what it is?
+             <p> Watch video here</span></span></h2>
+            </a>
+          </li>
+          </ul>
+     
+          <ul class="img-list">
+          <li>
+             <a href="https://www.youtube.com/watch?v=OFEwPwdIKW0">
+              <img src="images/image1.png"/>
+              <h2 class="header-font-style"> Learn More
+              <span class="text-content"><span>Don't know what it is?
+              <p> Watch video here</span></span></h2>
+             </a>
+          </li>  
+          <li>
+             <p class="content-font-style">We offer real-time analytics and reporting on various platforms.</p>
+          </li>
+         <li>
+            <a href="sap-hana.php">
+             <img src="images/hana.png"/>
+             <h2 class="header-font-style">SAP HANA
+             <span class="text-content"><span>CLICK HERE TO KNOW HOW WE CAN BENEFIT YOU</span></span></h2>
+            </a>
+          </li>
+          </ul>
+     
+          <ul class="img-list">
+          <li>
+             <a href="sap-ams.php">
+              <img src="images/ams.png"/>
+             <h2 class="header-font-style"> SAP AMS
+              <span class="text-content"><span>CLICK HERE TO KNOW HOW WE CAN BENEFIT YOU</span></span></h2>
+             </a>
+          </li>  
+          <li>
+             <p class="header-font-style">We offer unique and innovative solutions for managing your application IT estate efficiently.</p>
+          </li>
+         <li>
+            <a href="sap-erp.php">
+             <img src="images/image1.png"  />
+             <h2 class="header-font-style"> Learn More
+             <span class="text-content"><span>Don't know what it is?
+             <p> Watch video here</span></span></h2>
+            </a>
+          </li>
+          </ul>
+       
+          <ul class="img-list">
+          <li>
+             <a href="https://www.youtube.com/watch?v=r6DfkL8qM6U">
+              <img src="images/image1.png"/>
+             <h2 class="header-font-style"> Learn More
+             <span class="text-content"><span>Don't know what it is?
+             <p> Watch video here</span></span></h2>
+             </a>
+          </li>  
+          <li>
+             <p class="header-font-style">We offer a variety of customizable SAP RDS options to get you up and running implementations quickly.</p>
+          </li>
+         <li>
+            <a href="sap-rds.php">
+             <img src="images/rds.png"  />
+             <h2 class="header-font-style">SAP RDS
+             <span class="text-content"><span>CLICK HERE TO KNOW HOW WE CAN BENEFIT YOU</span></span></h2>
+            </a>
+          </li>
+          </ul>
+       </div>
+   <?php include'footer1.php'; ?>
